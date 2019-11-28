e13c148674498084293d5b7a0299e446e0f91c95
JOnas <Jones16511@hotmail.com>
2019-11-28 15:14:33 +0100

    changed port

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-const port = process.env.port || "3001";
+const port = process.env.PORT || "3001";
 
 app.listen(port, (err) => {
   if (err) {
